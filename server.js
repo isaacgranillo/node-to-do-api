35fec45d08a65f725fb33f1f80dbe4ca43eb2384
Isaac Granillo <isaac.granillo@gmail.com>
2016-04-05 16:07:58 -0700

    refactored server.js with underscore.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 1337;
@@ -18,13 +19,16 @@ app.get('/todos', function (req, res){
 
 app.get('/todos/:id', function (req, res){
 	var todoID = parseInt(req.params.id, 10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos, {id: todoID});
+/* replaced with underscore
+	// var matchedTodo;
 
-	todos.forEach(function (todo){
-		if (todoID === todo.id){
-			matchedTodo = todo;
-		}
-	});
+	// todos.forEach(function (todo){
+	// 	if (todoID === todo.id){
+	// 		matchedTodo = todo;
+	// 	}
+	// });
+*/
 	if (matchedTodo){
 		res.json(matchedTodo);
 	}
@@ -34,7 +38,14 @@ app.get('/todos/:id', function (req, res){
 });
 
 app.post('/todos', function (req, res){
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed'); //does not allow for unwanted fields to be stored
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+
 	body.id = todoNextId;
 	todoNextId++;
 	todos.push(body);
